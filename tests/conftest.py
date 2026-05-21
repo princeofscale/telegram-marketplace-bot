f7e93c2074cbd946439c2ea6253485b12abdfb0d
princeofscale <princeofscale@iMac-princeofscale.local>
2026-05-21 12:29:43 +0400

    Fix CI test environment and dependency updates

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+import os
+
+os.environ.setdefault("BOT_TOKEN", "123456789:TEST_TOKEN")
+os.environ.setdefault("SUPPORT_URL", "https://t.me/test_support")
+os.environ.setdefault("INVENTORY_ENCRYPTION_KEY", "test-inventory-encryption-key")
+os.environ.setdefault("AMPLITUDE_API_KEY", "")
